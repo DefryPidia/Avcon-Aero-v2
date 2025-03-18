2bb3f8c08a569f34640e507c303b80d5a75304f5
FeilaHerlambang <websitefeila@gmail.com>
2025-03-19 05:07:01 +0700

    [HA] Update font & image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,26 @@
 export default function Home() {
   return (
     <>
-      <div className="position-relative overflow-hidden home-section" >
-        <video className="position-absolute top-0 start-0 h-100 w-100 object-fit-cover" autoPlay muted loop playsInline>
-          <source src="https://video.wixstatic.com/video/9b43b8_f183b01216004f618c8724974c137e9b/720p/mp4/file.mp4" type="video/mp4" />
-        </video>
+      <div className="position-relative overflow-hidden home-section" 
+           style={{ backgroundImage: "url('https://s3-alpha-sig.figma.com/img/dcd6/7343/dd2752706f837f834c6ecfc18b766ca2?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Wxvrkboh7YoZ9TcLPiyAQXbphdVL7MVt3BgMqehe6NlvOzWBAr2ScjPL4oWbbW~PHddDZIh6mZ~IQmCvA6F0xk7I-GiBB1xCSl9WEr4sx3Y~3EE50IH46t51V9PX8PP1JbXL7g2F8elx2rmTr3M7~WitkvllsysFeZAG8SgrYqdLFRcGM0nuELvXYnZRcpQrsORDVTWOXnpBR-IAgYchd6imT9XavkKtK1rYOVw2RStcih0JPc2S4K5NUvOHUqu95pgNK4eyk8vEWskugufPp2UW2748W~esxMVlMOQMz9OE4AhBfZD1TJf9BaOIdTzPG9Gy9odHbkkGnI8dS7Cr6Q__')", backgroundSize: "cover", backgroundPosition: "center" }}>
 
-        <div className="container h-100 position-relative" style={{ zIndex: 1 }}>
+        <div className="position-absolute top-0 start-0 w-100 h-100" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", zIndex: 0 }}></div>
+        {/* <video className="position-absolute top-0 start-0 h-100 w-100 object-fit-cover" autoPlay muted loop playsInline>
+          <source src="https://video.wixstatic.com/video/9b43b8_f183b01216004f618c8724974c137e9b/720p/mp4/file.mp4" type="video/mp4" />
+        </video> */}
+        <div className="container h-100 position-relative" style={{ zIndex: 1, right: "7%", bottom: "10%" }}>
           <div className="row h-100 align-items-center ps-3">
             <div className="col-lg-12 text-start">
+              <img src="/image/Avcon-Aero-2-1.png"/>
               <h1 className="display-1 fw-bold mb-4 text-light position-relative">
                 KEEP YOU<br/>
                 BUSTLING IN THE SKY
               </h1>
               <p className="fs-5 fw-semibold text-light display-3 position-relative">
-              From high-quality aircraft parts to comprehensive aviation solutions <br/>
-              we keep your operations running smoothly. Contact us today!
+              From High-Quality Aircraft Parts To Comprehensive Aviation Solutions <br/>
+              We Keep Your Operations Running Smoothly. Contact Us Today!
               </p>
-              <a href="" className="btn btn-primary btn-lg px-5 py-3 fs-5 fw-bold" style={{ background: "linear-gradient(to right, darkblue, lightblue)", border: "none" }} >Request quote</a>
+              <a href="" className="btn btn-primary btn-lg px-5 py-3 fs-5 fw-bold btn-aero" >Request quote</a>
             </div>
           </div>
         </div>
@@ -31,10 +34,10 @@ export default function Home() {
           backgroundPosition: "center"
         }}
       >
-        <div className="position-absolute top-50 end-25 translate-middle-y" style={{ right: "calc(25% - 50vh)", width: "105vh", height: "70vh" }}>
+        <div className="position-absolute top-50 end-25 translate-middle-y" style={{ right: "calc(30% - 50vh)", width: "70vh", height: "50vh" }}>
           <div className="position-relative h-100 w-100">
             <img
-              src="https://static.wixstatic.com/media/9b43b8_1ba389f03d8941fab904a175a425c9df~mv2.png/v1/fill/w_693,h_441,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9b43b8_1ba389f03d8941fab904a175a425c9df~mv2.png"
+              src="/image/Asset-Avcon-frame-2.svg"
               alt="Aviation Services"
               className="position-absolute h-100 w-100 object-fit-cover"
             />
@@ -57,7 +60,7 @@ export default function Home() {
                 delivering reliable solutions for even the most specialized <br/>
                 components
               </p>
-              <a href="" className="btn btn-primary btn-lg ms-3 px-5 py-3 fs-5 fw-bold" style={{ background: "linear-gradient(to right, darkblue, lightblue)", border: "none" }} >Read More</a>
+              <a href="" className="btn btn-primary btn-lg ms-3 px-5 py-3 fs-5 fw-bold btn-aero" >Read More</a>
             </div>
           </div>
         </div>
@@ -223,7 +226,7 @@ export default function Home() {
                 We only use high-quality components to restore and enchance your <br/>
                 equipment, ensuring optimal performance and reliability
               </p>
-              <a href="" className="btn btn-primary btn-lg ms-3 px-5 py-3 fs-5 fw-bold w-25"  style={{ background: "linear-gradient(to right, darkblue, lightblue)", border: "none" }} >
+              <a href="" className="btn btn-primary btn-lg ms-3 px-5 py-3 fs-5 fw-bold w-25 btn-aero" >
                 Read More
               </a>
             </div>
@@ -288,7 +291,7 @@ export default function Home() {
       </div>
 
       <div className="container my-5">
-        <div className="d-flex flex-column flex-md-row align-items-center p-5 text-white vision-card" >
+        <div className="d-flex flex-column flex-md-row align-items-center p-5 text-white vision-card position-relative">
           <div className="vision-card-background"></div>
           <div className="vision-card-content">
             <h2 className="fw-bold">Driven By Quality, <br/> Safety And Care</h2>
@@ -298,6 +301,13 @@ export default function Home() {
             <a href="#" className="btn btn-danger fw-bold px-4 py-2">
               Get In Touch Now
             </a>
+          </div>
+
+          <div className="position-absolute" style={{ zIndex: 2, right: 0 }}>
+            <img src="/image/asset-avcon-frame-6.svg" alt="" className="img-fluid"  />
+          </div>
+          <div className="position-absolute" style={{ zIndex: 1, right: 0, bottom: 0 }}>
+            <img src="/image/asset-avcon-frame-6.1.svg" alt="" className="img-fluid"  />
           </div>
         </div>
       </div>
