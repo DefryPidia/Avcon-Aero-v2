47fec4020124d287db3208c3e4d7cc1d4d8ab4fc
DefryPidia <realdefrifidia@gmail.com>
2025-03-20 00:22:15 +0700

    feat: create footer and implement about us contents

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -16,47 +16,24 @@ const AboutUs = () => {
 
       <div className="container-fluid px-0">
         <div className="container col-md-8">
-          <p className="fw-bold text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          </p>
-          <p className="fw-bold text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          </p>
-        </div>
-
-        <div className="bg-secondary break-line my-5" />
-
-        <div className="container col-md-8">
-          <h2 className="display-4 fw-bold mb-4">Lorem Ipsum</h2>
-          <p className="fw-bold">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          <p className="text-justify">
+            PT Avcon Angkasa Semesta, a leading provider of aviation solutions. We take pride in delivering on time and services to our customers worldwide.<br />
+            At PT Avcon Angkasa Semesta, we are committed to excellence and customer satisfaction, We provide top-tier spare part sales, supported by our team of experts who are ready to offer technical assistance and guidance to our clients. <br />
+            With our experience and dedication to the industry, we are committed to being a trusted partner for our customers. We look forward to continuing to serve and meet your needs in the future.
           </p>
         </div>
 
-        <div className="my-5 bg-secondary transparent-line"></div>
+        <div className="container col-md-8 my-5">
+          <h2 className="display-5 fw-bold mb-4 aero-blue text-uppercase">The Purpose</h2>
+          <p className="text-justify">
+            The goal is to establish a leading position in the aviation sector, initially focusing on attaining leadership in Asia and expanding globally. The company is committed to excellence, innovation, and superior performance across all aspects of the aviation industry. The aim is not only to succeed in the competitive Asian market but also to become a recognized global entity. Additionally, the company strives to provide robust support for the aviation industry, actively engaging in initiatives, collaborations, and innovations to enhance various aspects and contribute to its growth and progression.
+          </p>
+        </div>
 
-        <div className="container col-md-8">
-          <h2 className="display-4 fw-bold mb-4">Lorem Ipsum</h2>
-          <p className="fw-bold">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          </p>
-          <p className="fw-bold">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          </p>
-          <p className="fw-bold">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        <div className="container col-md-8 my-5">
+          <h2 className="display-5 fw-bold mb-4 aero-blue text-uppercase">Aerospace aftermarket specialists</h2>
+          <p className="text-justify">
+            At the core of our commitment lies an unwavering dedication to addressing our clients' needs with utmost precision while upholding the pinnacle of safety and service standards. Collaborating closely with leading OEMs within the industry enables us to curate an unparalleled inventory of aircraft parts, ensuring precise compatibility for your specific aircraft model. Our comprehensive suite of offerings encompasses aircraft spare parts procurement, repair and overhaul services, bespoke aircraft chartering, alongside specialized aviation consultancy catering to both commercial and private aircraft operators.
           </p>
         </div>
 
--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,52 +1,66 @@
+import Image from "next/image";
+import AvconAeroImage from "@public/image/avcon-aero-white.svg"
+import EmailIcon from "@public/icon/email.svg"
+import MobilePhoneIcon from "@public/icon/mobile-phone.svg"
+import LinkedinIcon from "@public/icon/linkedin.svg"
+
 const Footer = () => {
     return (
-        <footer className="footer text-center text-md-start text-white bg-dark py-4 mt-auto">
-            <div className="container">
+        <footer style={{
+            background: "linear-gradient(to right, #d4112b 50%, #1d5fa8 50%)",
+            color: "white",
+            padding: "40px 0",
+        }}  >
+            <div className="container" style={{ padding: "0 40px 0 40px" }}>
                 <div className="row">
-                    <div className="col-md-4">
-                        <h4>Avcon Aero</h4>
-                        {/* <h4>Aero Supplies Global</h4> */}
-                        {/* <p>Experience the power of Aviation</p> */}
+                    <div className="col-md-8">
+                        <Image src={AvconAeroImage} alt="avcon aero" height={100} width={285} />
                     </div>
-
-                    {/* <div className="col-md-2">
-                        <h6>Company</h6>
-                        <ul className="list-unstyled">
-                            <li><a href="#" className="text-white text-decoration-none">About us</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">Location</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">ASG Careers</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">Events</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">Quality</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">Clients</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">Products</a></li>
-                        </ul>
-                    </div>
-
                     <div className="col-md-4">
-                        <p className="mb-1"><strong>Aero Supplies Global DWC LLC</strong></p>
-                        <p className="mb-1">Ground Floor, Supplier Complex, DWC - Mohammed Bin Rashid Aerospace Hub, United Arab Emirates</p>
-                        <p className="mb-1"><strong>UAE | INDIA | RWANDA | USA</strong></p>
-                        <p className="text-danger mb-1">24/7 AOG Desk:</p>
-                        <p className="mb-1">+971 65263464</p>
-                        <p className="mb-0">sales@aeroasg.com</p>
-                    </div> */}
+                        <div className="text-end">
+                            <div className="fw-bold">Avcon Headquarter</div>
+                            HQ Bella Terra Lifestyle CenterJI. Boulevard Raya Kav. 1 Kel. Kelapa Gading Timur, Kec. Kelapa Gading, Jakarta Utara 14240
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-3">
+                    <div className="col-md-6 d-flex flex-column justify-content-end">
+                        <div>
+                            <div className="d-flex align-items-center">
+                                <Image src={EmailIcon} height={24} width={24} className="me-1" />
+                                contact@avconaero.com
+                            </div>
+                            <div className="d-flex align-items-center">
+                                <Image src={MobilePhoneIcon} height={24} width={24} className="me-1" />
+                                +62 818-0740-0663
+                            </div>
+                            <div className="d-flex align-items-center">
+                                follow us on :
+                                <Image src={LinkedinIcon} height={80} width={106} className="ms-3" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 d-flex justify-content-end">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15866.208209901744!2d106.878961!3d-6.1906422!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4df4dd3a493%3A0x54d601d5b41b42f7!2sBella%20Terra%20Lifestyle%20Center%20Kelapa%20Gading!5e0!3m2!1sen!2sid!4v1738897062640!5m2!1sen!2sid"
+                            width="295"
+                            height="180"
+                            style={{
+                                borderRadius: "20px"
+                            }}
+                            allowFullScreen=""
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        ></iframe>
+                    </div>
                 </div>
 
-                {/* <hr className="bg-light" />
+                <div className="bg-white my-3" style={{ height: '1px' }}></div>
 
-                <div className="row align-items-center">
-                    <div className="col-md-6">
-                        <p className="mb-0">Follow us on <span className="fw-bold bg-white text-dark px-2 rounded">LinkedIn</span></p>
-                    </div>
-                    <div className="col-md-6 text-md-end">
-                        <p className="mb-0">© 2024 by Aero Supplies Global, All rights reserved.</p>
-                        <p className="mb-0">
-                            <a href="#" className="text-white text-decoration-none">Terms of Service</a> |
-                            <a href="#" className="text-white text-decoration-none">Privacy Policy</a> |
-                            <a href="#" className="text-white text-decoration-none">Cookie Policy</a>
-                        </p>
-                    </div>
-                </div> */}
+                <div className="d-flex justify-content-between">
+                    <span>© 2025 by Avcon Aero, All rights reserved.</span>
+                    <span>Term of services | Privacy Policy | Cookie Policy</span>
+                </div>
             </div>
         </footer>
     );
